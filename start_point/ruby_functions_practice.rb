38f1164e52ca935e5ad5bcca9c0137e023f968c6
Simon Atkins <this-is-simon@github.com>
2018-03-06 16:55:13 +0000

    The functions homework by Simon & Rachael

---
+++ start_point/ruby_functions_practice.rb
@@ -0,0 +1,31 @@
+def return_10
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_num, second_num)
+  return first_num - second_num
+end
+
+def multiply(first, second)
+  return first * second
+end
+
+def divide(first, second)
+  return first / second
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string1, string2)
+  return string1 + string2
+end
+
+def add_string_as_number(string1, string2)
+  return string1.to_i + string2.to_i
+end
